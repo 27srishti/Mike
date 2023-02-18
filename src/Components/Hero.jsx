eac06e24b3c36d11191d8e9c348d5e6d3ee93dac
srishti suratkal <279srishti@gmail.com>
2023-02-18 20:00:06 +0530

    upd1

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,315 @@
+import React from "react";
+// import One from "../Images/One.jpg"
+// import Two from "../Images/Two.jpeg"
+// import Three from "../Images/Three.png"
+
+const Hero = () => {
+  return (
+    <div className="flex grid-cols-2  ">
+      {/* ------------------sidebar------------------------------ */}
+
+      <aside
+        id="separator-sidebar"
+        className=" z-40  hidden   lg:flex lg:block transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-screen w-54 px-3 py-4  overflow-y-auto bg-gray-50 ">
+          <ul className="space-y-2">
+            <li>
+              <a
+                href="#"
+                className="flex text-center items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 d"
+              >
+                <span className=" w-full h-9 border-gray-400 border-b">
+                  Search By Make
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  APC
+                  <option>APC</option>
+                  <option>Automatic Transfer Switch</option>
+                  <option>PDU - 120V</option>
+                  <option>PDU - 208V</option>
+                  <option>PDU - 3 Phase</option>
+                  <option>Symmetra</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1  px-3 p-0 m-0 border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Arista
+                  <option> Arista</option>
+                  <option>7048</option>
+                  <option>7050</option>
+                  <option>7050SX</option>
+                  <option>7050TX</option>
+                  <option>7050Q</option>
+                  <option>7050X3</option>
+                  <option>7060X</option>
+                  <option>7100</option>
+                  <option>7150</option>
+                  <option>7160</option>
+                  <option>7170</option>
+                  <option>7250QX</option>
+                  <option>7260X </option>
+                  <option>7280E</option>
+                  <option>7280R</option>
+                  <option>7300X</option>
+                  <option>7320X</option>
+                  <option>7368X4</option>
+                  <option>7500</option>
+                  <option>7500R</option>
+                  <option>Arista Transceiver </option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Aruba
+                  <option> Aruba</option>
+                  <option> 2530</option>
+                  <option>2540</option>
+                  <option>2920</option>
+                  <option>2930F</option>
+                  <option> 2930M</option>
+                  <option>3810</option>
+                  <option> 5400R zl2</option>
+                  <option> 7200</option>
+                  <option> 7220</option>
+                  <option>8320</option>
+                  <option> CX 8325</option>
+                  <option>s1500</option>
+                  <option>s3500</option>
+                  <option>X371</option>
+                  <option>Transceivers</option>
+                  <option>Wireless AP</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Brocade
+                  <option>Brocade</option>
+                  <option>300 SAN Switch</option>
+                  <option>5100 SAN Switch</option>
+                  <option>5300 SAN Switch</option>
+                  <option>6500 SAN Switch</option>
+                  <option>8000 SAN Switch</option>
+                  <option>BigIron RX</option>
+                  <option>FastIron Edge X</option>
+                  <option>FastIron GS</option>
+                  <option>FastIron SX</option>
+                  <option>FCX</option>
+                  <option>ICX</option>
+                  <option>MLX</option>
+                  <option>NetIron CER</option>
+                  <option>NetIron CES</option>
+                  <option>NetIron XMR</option>
+                  <option>ServerIron</option>
+                  <option>TurboIron</option>
+                  <option>VDX Switches</option>
+                  <option>Optical Transceivers</option>
+                </select>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Cisco
+                  <option>Cisco </option>
+                  <option>Catalyst Switches</option>
+                  <option>Nexus Switches</option>
+                  <option>Industrial Ethernet Switches</option>
+                  <option>MDS Storage Switches</option>
+                  <option>ME Ethernet Access</option>
+                  <option>Small Business Switches</option>
+                  <option>Firewalls</option>
+                  <option>Routers</option>
+                  <option>Transceivers</option>
+                  <option>VoIP Phones</option>
+                  <option>Wireless</option>
+                  <option>UCS Servers</option>
+                  <option>Cisco Rack Mount Brackets</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Cisco Meraki
+                  <option>Meraki Firewalls</option>
+                  <option>Meraki Switches</option>
+                  <option>Meraki Wireless</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Dell
+                  <option>Dell</option>
+                  <option>PowerSwitch</option>
+                  <option>PowerConnect</option>
+                  <option>X-Series</option>
+                  <option>PowerEdge</option>
+                  <option>EqualLogic</option>
+                  <option>Dell Power Cord</option>
+                  <option>EMC Storage</option>
+                  <option>Transceivers</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Extreme
+                  <option>Extreme</option>
+                  <option>X430</option>
+                  <option>X450a</option>
+                  <option>X440</option>
+                  <option>X440-G2</option>
+                  <option>X450E</option>
+                  <option>X450-G2</option>
+                  <option>X460</option>
+                  <option>X460-G2</option>
+                  <option>X480</option>
+                  <option>X620</option>
+                  <option>X650</option>
+                  <option>X670</option>
+                  <option>X770</option>
+                  <option>8800</option>
+                  <option>8900</option>
+                  <option>ERS 4900</option>
+                  <option>MLX</option>
+                  <option>SLX 9030</option>
+                  <option>SLX 9140</option>
+                  <option>SLX 9240</option>
+                  <option>SLX 9540</option>
+                  <option>VDX</option>
+                  <option>VSP4000</option>
+                  <option>VSP4900</option>
+                  <option>VSP7400</option>
+                  <option>VSP8200</option>
+                  <option>Altitude 4600</option>
+                  <option>Extreme Transceiver</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  HPE
+                  <option>HPE</option>
+                  <option>HPE Switches</option>
+                  <option>HPE Transceivers</option>
+                  <option>Proliant</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                  Intel
+                  <option>Intel</option>
+                  <option>Xeon E3 V3</option>
+                  <option>Xeon E3 V5</option>
+                  <option>Xeon E5 V2</option>
+                  <option>Xeon E5 V3</option>
+                  <option>Xeon E5 V4</option>
+                  <option>Xeon E7 V2</option>
+                  <option>Xeon E7 V3</option>
+                  <option>Xeon E7 V4</option>
+                  <option>Network Adapters</option>
+                </select>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center text-sm font-normal text-gray-500 rounded-lg"
+              >
+                <select className="flex-1 px-3 p-0 m-0  border-none focus:ring-0 bg-gray-50 border-0 whitespace-nowrap">
+                Juniper
+                Juniper
+                E Series
+EX Switches
+
+M Edge Routers
+MX Routers
+Pulse
+QFX Switches
+
+PTX Routers
+SRX Gateways
+SSG Gateways
+STRM
+T Core Routers
+Transceivers
+                </select>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      {/* --------------------------Hero Section------------------------------------------------------ */}
+
+      <div className="">
+        <section className="img-slider overflow-hidden  ">
+          <div className="slider-container  rounded-lg  ">
+            <div className="slide">
+              <img src="https://images.pexels.com/photos/3251700/pexels-photo-3251700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+              {/* <img src={One} /> */}
+            </div>
+            <div className="slide">
+              <img src="https://images.pexels.com/photos/3780664/pexels-photo-3780664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+              {/* <img src={Two}/> */}
+            </div>
+            <div className="slide">
+              <img src="https://images.pexels.com/photos/747079/pexels-photo-747079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+              {/* <img src={Three} /> */}
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import Logo from "../Images/Logo.png";
+
+const Navbar = () => {
+  return (
+    <section>
+      <div className="  text-sm text-center  bg-gray-700 text-white justify-between flex grid-cols-2 mx-auto bg-opacity-90 h-7">
+        <ui className="flex grid-cols-4 ">
+          <div className=" px-10 flex items-center bg-lime-600">Store</div>
+          <div className=" border-l flex items-center bg-gray-500 px-10">
+            Blog
+          </div>
+          <div className=" border-l flex items-center bg-gray-500 px-10">
+            News
+          </div>
+        </ui>
+        <ui className="flex grid-cols-4 hidden lg:flex lg:block">
+          <div className=" px-10 flex items-center bg-gray-500">
+            1-800-430-6950
+          </div>
+          <div className=" border-l flex items-center bg-amber-600 px-10">
+            Ask For Quote
+          </div>
+        </ui>
+      </div>
+      {/* -----------------------------------TopNav-End---------------------------------------------------------- */}
+      <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900  w-full z-20  border-b border-gray-200 dark:border-gray-600">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <a href="" className="flex items-center">
+            <img src={Logo} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+          </a>
+
+          <div className="flex md:order-2 mx-2 md:mx-0">
+            <a
+              href="#_"
+              class="relative rounded px-5 py-2.5 text-sm  overflow-hidden group bg-lime-600 relative hover:bg-gradient-to-r hover:from-lime-700 hover:to-lime-600 text-white hover:ring-2 hover:ring-offset-2 hover:ring-lime-400 transition-all ease-out duration-300"
+            >
+              <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+              <span class="relative">Log-In</span>
+            </a>
+            <img
+              className=" cursor-pointer mx-5 hidden lg:block lg:flex md:w-10 opacity-60  "
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAACJUlEQVR4nO2Yz0sVURTHP6/iEUaJuJQg3ISQpFCJLVzZqhbiIghc2Kq/oJUREVIvonARtDF3BUaRtAlSoV1BtWkpREQ9RQzbiAbSe3HhBl8uz3Lu3LnjwHzgwJt5c77n3Llzf5wLJSUlJXuFW8A20LS2AbwFLgIVCsCGJO9ajQL2QNOxsxSMw8AzacA0BWRAGrC2wzMzwO9/9FwIWweeA8eSNqDNETLXSnvGiTcd+wEMJm3EmggM5NQDTbEPSWfFx+J8hXw44bzIRBPKVXF8SH7MSh5mbdo1w+L4jvx4L3kMJXHsFMdNYD/xOQBs2RwaQEdSgW/SiB7i0yvxv/gIvBSBS8RnTOK/8BG4KQJ3iM9diX/DR2BUBF4Tn3mJP+Ij0O2shrFZlfiJtxPYlW9dRLqIR5fE/ZmmNnkjQheIx3mJa3LwZkqErhGPCYlrcvBmXIRMnRCLpxL3chqhkyL0mXgsSdz+NEJV4Jcs56YWyJpDslU35e7BtIIf5W2cI3uGJN6nEIL3RHAxwsZuTuI9CCF43Dm5WABO291iSI4CTySO+WT7QolfD1gergCnRHvS3ms4z6WaPl0qthF/B3Rau291j7T4r2GrwEw+VbM/egR8/c+B2G57oAK8sve37O8zFJAOYF/eSRSOqi10loG6PQyuZugXnJrniXbN0y84yzsM0Kz8glNvkcj3DP2C0+pTuJ2hX3CqNpm6xyCuefiVlJSUsDf4Axp2YWimEJkBAAAAAElFTkSuQmCC"
+            ></img>
+          </div>
+          <div className=" md:w-full flex justify-between  h-[100%] hidden max-w-xs  md:max-w-xl xl:max-w-3xl 2xl:max-w-5xl  border-[0.5px] border-gray-900 border-opacity-50 rounded-sm  xl:flex items-center">
+            <input
+              className=" border-none focus:ring-0 w-full border-none font-normal text-sm pl-6"
+              type="text"
+              placeholder="Find what you need..."
+            />
+            <button className=" bg-lime-600 w-24 h-10  text-white ">
+              {" "}
+              Search
+            </button>
+            {/* <svg
+            className="ml-auto h-5 px-5   "
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="far"
+            data-icon="search"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              className=""
+              fill="currentColor"
+              d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"
+            />
+          </svg> */}
+          </div>
+        </div>
+      </nav>
+      {/* -------------------------------------------MiddleNav-End---------------------------------------------- */}
+      <nav className="bg-lime-600 text-white text-xs sm:text-sm md:text-sm  lg:text-sm font-medium ">
+        <div className="container flex items-center text-center  justify-center py-5 mx-auto  capitalize ">
+          <a
+            href="#"
+            className="border-b-2 border-transparent hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="border-b-2 border-transparent hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            All Products
+          </a>
+          <a
+            href="#"
+            className="border-b-2 hidden lg:flex lg:block border-transparent text-center hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            {" "}
+            Terms Of sale
+          </a>
+          <a
+            href="#"
+            className="border-b-2 border-transparent hidden lg:flex lg:block hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            Shipping Policy
+          </a>
+          <a
+            href="#"
+            className="border-b-2 border-transparent hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            About Us
+          </a>{" "}
+          <a
+            href="#"
+            className="border-b-2 border-transparent hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            Contact Us
+          </a>{" "}
+          <a
+            href="#"
+            className="border-b-2 border-transparent hidden lg:flex lg:block hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            FAQ's
+          </a>
+          <a
+            href="#"
+            className="border-b-2 border-transparent hidden lg:flex lg:block hover:text-gray-900  hover:border-lime-500 mx-3 sm:mx-6"
+          >
+            Netletter
+          </a>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
