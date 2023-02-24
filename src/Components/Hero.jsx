6ff1d49ce8ad2a2d29f4c11559c5323fac5eccb5
srishti suratkal <279srishti@gmail.com>
2023-02-24 20:41:36 +0530

    upd17

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -398,7 +398,7 @@ const Hero = () => {
               With Quality Switches, Routers, Firewalls And PDU's.
             </span>
             <div className="mt-7 ">
-            <a href="#_" className="relative inline-flex items-center justify-start px-3 py-3 overflow-hidden w-28 font-medium transition-all bg-green rounded hover:bg-green group">
+            <a href="https://www.networktigers.com/pages/all-products" className="relative inline-flex items-center justify-start px-3 py-3 overflow-hidden w-28 font-medium transition-all bg-green rounded hover:bg-green group">
   <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
   <span className="relative w-full text-left text-white text-center transition-colors duration-300 ease-in-out font-semibold text-sm group-hover:text-black">Explore More</span>
 </a>
