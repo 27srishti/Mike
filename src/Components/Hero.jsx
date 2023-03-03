e3ff01313e11537e3ea23e5fb30a80325b6a442b
srishti suratkal <279srishti@gmail.com>
2023-03-03 23:19:07 +0530

    done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react";
 import One from "../Images/One.jpg";
 import Two from "../Images/Two.jpg";
 import Three from "../Images/Three.png";
-
+import Text1 from "../HeroText/Text1";
+import Text2 from "../HeroText/Text2";
+import Text3 from "../HeroText/Text3";
 const images = [One, Two, Three];
-
-
-
 const Hero = () => {
   // --------------------image change function ----------------------------
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,7 +21,7 @@ const Hero = () => {
 
     return () => clearInterval(intervalId);
   }, [currentIndex]);
-  // --------------------image change function end ----------------------------
+
   return (
     <div className="flex grid-cols-2  ">
       {/* ------------------sidebar------------------------------ */}
@@ -384,36 +383,14 @@ const Hero = () => {
       {/* --------------------------Hero Section------------------------------------------------------ */}
 
       <div className="slide  ">
-        <div className=" mt-[10%] sm:mt-14  md:mt-[10%] w-[90%] pl-[10%]  absolute my-auto mx-auto ">
-          <div class="relative  flex md:w-[36rem] 2xl:w-[54rem]  items-center">
-            <div class=" w-5 mt-1 md:w-10  2xl:w-32 border-t border-[3px] border-green"></div>
-            <span class="flex-shrink px-2 text-md  font-light  mx-auto  text-white 2xl:text-4xl  md:text-2xl">
-              Networks Are Everywhere
-            </span>
-            <div class="flex-grow  mt-2 border-t  border-[3px] border-green"></div>
-          </div>
+        {currentIndex == 0 ? (
+          <Text1 />
+        ) : currentIndex === 1 ? (
+          <Text2 />
+        ) : (
+          <Text3 />
+        )}
 
-          <h1 className="mt-3  md:w-[42rem] 2xl:leading-[3rem] 2xl:mt-5  2xl:w-[60rem] 2xl:text-4xl text-xl  font-semibold absolute  mx-auto  text-white  md:text-2xl ">
-            {" "}
-            NetworkTigers Support Network Managers Across All Networks
-            <span className="text-white ">
-              {" "}
-              With Quality Switches, Routers, Firewalls And PDU's.
-            </span>
-            <div className="mt-7 ">
-              <a
-                href="https://www.networktigers.com/pages/all-products"
-                className="relative inline-flex items-center justify-start px-3 py-3 overflow-hidden w-28 2xl:w-40 2xl:h-16 font-medium transition-all bg-green rounded hover:bg-green group"
-              >
-                <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
-                <span className="relative w-full text-left text-white text-center transition-colors duration-300 ease-in-out font-semibold  2xl:text-xl text-sm group-hover:text-black">
-                  Explore More
-                </span>
-              </a>
-            </div>
-          </h1>
-        </div>
-        
         <div className=" grid items-end   flex place-items-center ">
           <div className=" text-center  w-screen  text-white absolute bg-gray-500 py-2 text-xs  ">
             Since 1996, NetworkTigers has been providing quality to Netowrk
--- src/HeroText/Text2.jsx
+++ src/HeroText/Text2.jsx
@@ -1,44 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Text2 = () => {
   return (
     <section>
-      <div className=" mt-[20%]  md:mt-[10%] w-[90%] pl-[10%]  absolute my-auto mx-auto ">
-          <div class="relative  flex md:w-[36rem]  items-center">
-            <div class=" w-5 mt-1 md:w-10 border-t border-[3px] border-green"></div>
-            <span class="flex-shrink px-2 text-md  font-light  mx-auto  text-white  md:text-2xl">
-              Networks Are Everywhere
-            </span>
-            <div class="flex-grow  mt-2 border-t border-[3px] border-green"></div>
-          </div>
-
-          <h1 className="mt-3  md:w-[42rem]  text-xl  font-semibold absolute  mx-auto  text-white  md:text-2xl ">
-            {" "}
-            NetworkTigers  ndhedbuqebi Managers Across All Networks
-            <span className="text-white ">
-              {" "}
-              With Quality Switches, Routers, Firewalls And PDU's.
-            </span>
-            <div className="mt-7 ">
-              <a
-                href="https://www.networktigers.com/pages/all-products"
-                className="relative inline-flex items-center justify-start px-3 py-3 overflow-hidden w-28 font-medium transition-all bg-green rounded hover:bg-green group"
-              >
-                <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
-                <span className="relative w-full text-left text-white text-center transition-colors duration-300 ease-in-out font-semibold text-sm group-hover:text-black">
-                  Explore More
-                </span>
-              </a>
-              
-            </div>
-          </h1>
-          {/* <h1 className=" text-white   w-screen bg-gray-400 py-2 text-xs text-center ">
-            Since 1996, NetworkTigers has been providing quality to Netowrk
-            Managers around the world.
-          </h1> */}
+      <div className=" mt-[10%]  md:mt-[10%] w-[90%] pl-[10%]  absolute my-auto mx-auto ">
+        <div class="relative  flex md:w-[36rem]  items-center">
+          <div class=" w-5 mt-1 md:w-10 border-t border-[3px] border-green"></div>
+          <span class="flex-shrink px-2 text-md  font-light  mx-auto  text-white  md:text-2xl">
+            Networks Are Everywhere
+          </span>
+          <div class="flex-grow  mt-2 border-t border-[3px] border-green"></div>
         </div>
-    </section>
-  )
-}
 
-export default Text2
+        <h1 className="mt-3  capitalize md:w-[42rem]  text-xl  font-semibold absolute  mx-auto  text-white  md:text-2xl ">
+          {" "}
+          Since 1996, NetworkTigers has been offering fast shipment of
+          <span className="text-white ">
+            {" "}
+            quality seller refurbished equipment to governments, schools,
+            healthcare organizations, and businesses.
+          </span>
+          <div className="mt-7 ">
+            <a
+              href="https://www.networktigers.com/pages/all-products"
+              className="relative inline-flex items-center justify-start px-3 py-3 overflow-hidden w-28 font-medium transition-all bg-green rounded hover:bg-green group"
+            >
+              <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
+              <span className="relative w-full text-left text-white text-center transition-colors duration-300 ease-in-out font-semibold text-sm group-hover:text-black">
+                Explore More
+              </span>
+            </a>
+          </div>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Text2;
